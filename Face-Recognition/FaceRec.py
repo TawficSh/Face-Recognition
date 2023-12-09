4ba96261421a89a8e814501a65b046b31bcb2be1
TawficSh <145820498+TawficSh@users.noreply.github.com>
2023-12-09 17:17:50 -0500

    FaceRec.py

--- Face-Recognition/FaceRec.py
+++ Face-Recognition/FaceRec.py
@@ -1,4 +1,3 @@
-import tensorflow
 import cv2
 import numpy as np
 from mtcnn.mtcnn import MTCNN
@@ -11,31 +10,41 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+# Function to send an email notification with the attached image
 def send_email_with_image(image_path):
+    # Email details
     sender_email = "tawfiq.good1@gmail.com"
     sender_password = "cvrqbtywlkgnzfcb"
     receiver_email = "taw20200499@std.psut.edu.jo"
     subject = "Unknown Face Detected"
     body = "An unknown face was detected using your computer. Please find the attached image for reference."
 
+    # Creating a multipart message
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = subject
 
+    # Email Body
     message.attach(MIMEText(body, "plain"))
 
+    # Open the image that will be attached
     with open(image_path, "rb") as image_file:
+        # Attach the image to the email
         image_part = MIMEImage(image_file.read(), name="unknown_face.jpg")
         message.attach(image_part)
 
     text = message.as_string()
 
+    # Connecting to a SMTP server
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
 
     try:
+        # Log in to the server
         server.login(sender_email, sender_password)
+
+        # Send the email
         server.sendmail(sender_email, receiver_email, text)
         print("Email sent successfully!")
 
@@ -43,8 +52,10 @@ def send_email_with_image(image_path):
         print("An error occurred while sending the email:", str(e))
 
     finally:
+        # Disconnect from the server
         server.quit()
 
+#preprocessing images for face recognition
 def preprocess_image(image):
     image = cv2.resize(image, (160, 160))
     image = image.astype('float32')
@@ -53,6 +64,7 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
+# Creating generators for training with data augmentation
 batch_size = 32
 train_datagen = ImageDataGenerator(
     rescale=1./255,
@@ -65,6 +77,7 @@ train_datagen = ImageDataGenerator(
     fill_mode='nearest'
 )
 
+# Define the global train_generator variable
 train_generator = train_datagen.flow_from_directory(
     r'C:\Users\ACER\IdeaProjects\ece5831\Images',
     target_size=(160, 160),
@@ -75,47 +88,62 @@ train_generator = train_datagen.flow_from_directory(
 class_indices = train_generator.class_indices
 print("Class indices:", class_indices)
 
+# Recognition of faces in real-time using the camera
 def recognize_faces():
+    # loading MTCNN model for face detection
     detector = MTCNN()
-    face_recognition_model = load_model('path/to/your/model.h5')
+
+    face_recognition_model = load_model('Tawfic.h5')
 
     cap = cv2.VideoCapture(0)
+
+    # Flag to track whether an email has been sent
     email_sent = False
 
     while True:
         ret, frame = cap.read()
 
+        # Detect faces using MTCNN
         faces = detector.detect_faces(frame)
 
         for face in faces:
             x, y, w, h = face['box']
             roi = frame[y:y+h, x:x+w]
 
+            # Preprocess the face for recognition
             processed_face = preprocess_image(roi)
 
+            # Use the face recognition model to predict the label
             label = face_recognition_model.predict(processed_face)[0][0]
             print("Label:", label)
 
+            # Draw bounding box around the face
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            if label == 1:
+            # Display the name on the frame based on the predicted label
+            if label == 1: 
                 cv2.putText(frame, "Tawfic", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             else:
                 cv2.putText(frame, "Unknown", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
                 cv2.imwrite("unknown_face.jpg", frame)
 
+                # If an unknown face is detected and email has not been sent, send an email with the captured image
                 if not email_sent:
                     send_email_with_image("unknown_face.jpg")
                     email_sent = True
 
+        # Display the frame
         cv2.imshow('Face Recognition', frame)
 
+        # Press 'q' to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    # Releasing the camera and closing the window
     cap.release()
     cv2.destroyAllWindows()
 
+# Model architecture
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(160, 160, 3)))
 model.add(MaxPooling2D((2, 2)))
@@ -127,10 +155,13 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
+# Compiling the model
 model.compile(optimizer='adam', loss=tensorflow.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
-model.fit(train_generator, epochs=10)
+# Training the model
+model.fit(train_generator, epochs=10)  # Adjust the number of epochs as needed
 
-model.save('path/to/your/model.h5')
+# Saving the model
+model.save('Tawfic.h5')
 
 recognize_faces()
